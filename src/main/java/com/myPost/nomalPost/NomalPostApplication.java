845d1366eb5b39e70fbce2d8fa2b1b22b2c4c1bf
Hwan <76806770+sang-hwann@users.noreply.github.com>
2023-07-25 21:34:15 +0900

    create project

---
+++ src/main/java/com/myPost/nomalPost/NomalPostApplication.java
@@ -0,0 +1,13 @@
+package com.myPost.nomalPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomalPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomalPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myPost/nomalPost/NomalPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myPost.nomalPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomalPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
